25a0d65d0d76b2bb96a3d686c47eabf0ea813181
Michaël Lauwereys <michael.lauwereys@gmail.com>
2021-08-19 15:50:23 +0200

    Options UI

--- code/ui/Message.cs
+++ code/ui/Message.cs
@@ -12,10 +12,7 @@ public class Message : Panel
 
 	public Message()
 	{
-		TextBox = Add.Panel( "testbox" );
-		TextBoxText = TextBox.Add.Label( "Features in the works:\n    - Change player speeds\n    - Three random items you can spawn\n    - Checkpoint cooldown\n    - Show when levels are finished on the hud\n    - Custom gravgun model\n    - Point system", "value" );
-
 		Options = Add.Panel( "options" );
-		OptionsText = Options.Add.Label( "Options:\n    - Set checkpoint: 1\n    - Teleport to checkpoint: 2\n    - Remove checkpoint: 9\n    - Thirdperson: c", "value" );
+		OptionsText = Options.Add.Label( "1 - Set checkpoint\n2 - Teleport to checkpoint\n9 - Remove checkpoint\nc - Thirdperson", "value" );
 	}
 }
